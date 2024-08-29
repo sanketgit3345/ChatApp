b0a895c4c6a628a185d36589d36934b2bef5295d
sanket434 <130990008+sanket434@users.noreply.github.com>
2024-08-29 23:20:39 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,98 @@
+const express = require("express")
+const dotenv= require("dotenv");
+const connectDB = require("./config/db");
+const userRoutes=require("./routes/userRoutes")
+const chatRoutes= require("./routes/chatRoutes")
+const messageRoutes= require("./routes/messageRoutes")
+const path= require("path")
+
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+
+
+dotenv.config();
+connectDB()
+const app=express();
+
+
+
+app.use(express.json());
+
+app.use("/api/user", userRoutes);
+ app.use("/api/chat", chatRoutes);
+ app.use("/api/message", messageRoutes);
+
+
+//---------------------------deplay-----------------------------
+
+ const __dirname1=path.resolve();
+
+ if(process.env.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname1,"/frontend/build")))
+
+    app.get("*",(req,res)=>{
+       res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"))
+    })
+ }
+ else{
+    app.get("/",(req,res)=>{
+        res.send("Api is running")
+    })
+ }
+
+ //---------------------------deplay-----------------------------
+
+
+
+app.use(notFound);
+app.use(errorHandler);
+
+
+const PORT= process.env.PORT || 5000;
+
+const server=app.listen(PORT,console.log(`server started at ${PORT}`));
+
+const io = require("socket.io")(server,{
+    pingTimeout: 60000,
+    cors:{
+        origin: "http://localhost:3000",
+    } 
+});
+
+io.on("connection",(socket)=>{
+    console.log("Connected");
+
+    socket.on('setup',(userData)=>{
+        socket.join(userData._id);
+        socket.emit("connected");
+    });
+
+    socket.on("join chat", (room)=>{
+        socket.join(room);
+        console.log("User joined Room: " + room);
+    });
+
+
+    socket.on("typing", (room)=>socket.in(room).emit("typing"));
+    socket.on("stop typing", (room)=>socket.in(room).emit("stop typing"));
+
+
+    socket.on("new message", (newMessageRecieved)=>{
+        var chat=newMessageRecieved.chat;
+
+        if(!chat.users){
+            return console.log("chat.users not defined");
+        }
+
+        chat.users.forEach(user=>{
+            if(user._id==newMessageRecieved.sender._id) return;
+
+            socket.in(user._id).emit("message recieved" , newMessageRecieved);
+        });
+    });
+    
+    socket.off('setup',()=>{
+        console.log("User Disconnected");
+        socket.leave(userData._id);
+    });
+
+});
